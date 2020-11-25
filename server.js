79a285357fe83c02606845666205386d68cfa2a1
ricardofonsecacampos <34239257+ricardofonsecacampos@users.noreply.github.com>
2020-11-25 07:08:05 -0300

    três serviços
    save + list + html

--- server.js
+++ server.js
@@ -1,12 +1,29 @@
 // server.js
+
 const http = require('http');
-const fs = require('fs')
+const fs = require('fs');
+const url = require('url');
+//const querystring = require('querystring');
+
 // use the port Heroku indicates
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'content-type': 'text/html' })
-  fs.createReadStream('poketrader-bxblue.html').pipe(res)
+  let pathname = url.parse(req.url,true).pathname
+  
+  if (pathname.indexOf('/save?') > -1) {
+    // gravar troca no banco.
+    console.log('Save one trade ' + url.parse(req.url,true).query);
+    
+  } else if (req.path.indexOf('/list') > -1) {
+    // mostrar histórico.
+    console.log('List trades ' + url.parse(req.url,true).search);
+    
+  } else {
+    // servir a página da aplicação de troca.
+    res.writeHead(200, { 'content-type': 'text/html' })
+    fs.createReadStream('poketrader-bxblue.html').pipe(res)
+  }
 }).listen(PORT);
 
 console.log('Node server running on port ' + PORT);
