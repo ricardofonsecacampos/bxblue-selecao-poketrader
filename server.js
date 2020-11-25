2e6d8b7faafd597d887082f69a85dd199bd002c5
ricardofonsecacampos <34239257+ricardofonsecacampos@users.noreply.github.com>
2020-11-24 21:44:19 -0300

    Update server.js
    Transformando em file server o NodeJS para testar o trader.

--- server.js
+++ server.js
@@ -1,17 +1,14 @@
 // server.js
 const http = require('http');
-
-// Create an instance of the http server to handle HTTP requests
-let app = http.createServer((req, res) => {
-    res.writeHead(301, {
-        Location: "http" + //(req.socket.encrypted ? "s" : "") +
-            "://" + req.headers.host + "/poketrader-bxblue.html"
-    });
-    res.end();
-});
-
-// Start the server on port Heroku indicates
+var static = require('node-static');
+var file = new static.Server();
+// use the port Heroku indicates
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, '0.0.0.0');
+http.createServer(function(request, response) {
+  request.addListener('end', function() {
+    file.serve(request, response);
+  }).resume();
+}).listen(PORT || 3000);
+
 console.log('Node server running on port ' + PORT);
